6a635f2145d862657f36e46b1c42189b2f75efc8
Kenny <kenny@acer.com>
2017-07-13 14:25:48 +0200

    Merge origin/master
    Conflicts:
    	public/js/app.js
    	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-use Illuminate\Support\Facades\Route;
-
 /*
  * |--------------------------------------------------------------------------
  * | Web Routes
