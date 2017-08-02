0e56f612a7b23d7f1c84aedc56cbd041dd831817
Kenny <kenny@acer.com>
2017-08-02 09:47:13 +0200

    Login form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/front.admin';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,38 +15,42 @@ use App\Utilities\UuidTools;
 use Illuminate\Support\Facades\DB;
 use View;
 
-class HomeController extends Controller
-{
-    public static function index(){
+class HomeController extends Controller {
+
+    public function __construct() {
+        $this->middleware('guest');
+    }
+
+    public static function index() {
         $distance = SearchController::DEFAULT_DISTANCE_KM_SEARCH;
         $sliderEts = array();
         $userLat = SessionController::getInstance()->getUserLat();
         $userLng = SessionController::getInstance()->getUserLng();
         $typeEts = SessionController::getInstance()->getUserTypeEts();
         $userLatLng = new LatLng($userLat, $userLng);
-        if(!$userLatLng->isValid()) {
+        if (!$userLatLng->isValid()) {
             $userLatLng = GeolocationController::getRawInitialGeolocation();
         }
         $sliderEtsQuery = Restaurant::where('id_business_type', '=', $typeEts)
-                            ;
+        ;
         $geolocLimitSuccess = DbQueryTools::setGeolocLimits($sliderEtsQuery, $userLatLng, $distance, Establishment::TABLENAME);
-        if($geolocLimitSuccess){
+        if ($geolocLimitSuccess) {
             $sliderEts = $sliderEtsQuery->get();
         }
-        
+
         $establishments = array();
         $establishmentsQuery = DB::table(Establishment::TABLENAME)
-                    ->select(DB::raw(Establishment::TABLENAME . '.*, ' . Address::TABLENAME . '.* '
-                                    . ', biz_category1.id as id_biz_category_1, biz_category1.name as name_biz_category_1'
-                                    . ',' . DbQueryTools::genRawSqlForDistanceCalculation($userLatLng, Establishment::TABLENAME)))
-                    ->join(Address::TABLENAME, Address::TABLENAME . '.id', '=', Establishment::TABLENAME . '.id_address')
-                    ->join(EstablishmentBusinessCategory::TABLENAME, Establishment::TABLENAME . '.id', '=', EstablishmentBusinessCategory::TABLENAME . '.id_establishment')
-                    ->join(BusinessCategory::TABLENAME . ' AS biz_category1', 'biz_category1.id', '=', EstablishmentBusinessCategory::TABLENAME . '.id_business_category')
-                    ->leftJoin(Promotion::TABLENAME, Establishment::TABLENAME . '.id', '=', Promotion::TABLENAME . '.id_establishment')
-                    ->where(Establishment::TABLENAME . '.id_business_type', '=', $typeEts)
-            ;
+                ->select(DB::raw(Establishment::TABLENAME . '.*, ' . Address::TABLENAME . '.* '
+                                . ', biz_category1.id as id_biz_category_1, biz_category1.name as name_biz_category_1'
+                                . ',' . DbQueryTools::genRawSqlForDistanceCalculation($userLatLng, Establishment::TABLENAME)))
+                ->join(Address::TABLENAME, Address::TABLENAME . '.id', '=', Establishment::TABLENAME . '.id_address')
+                ->join(EstablishmentBusinessCategory::TABLENAME, Establishment::TABLENAME . '.id', '=', EstablishmentBusinessCategory::TABLENAME . '.id_establishment')
+                ->join(BusinessCategory::TABLENAME . ' AS biz_category1', 'biz_category1.id', '=', EstablishmentBusinessCategory::TABLENAME . '.id_business_category')
+                ->leftJoin(Promotion::TABLENAME, Establishment::TABLENAME . '.id', '=', Promotion::TABLENAME . '.id_establishment')
+                ->where(Establishment::TABLENAME . '.id_business_type', '=', $typeEts)
+        ;
         $geolocLimitSuccess = DbQueryTools::setGeolocLimits($establishmentsQuery, $userLatLng, $distance, Establishment::TABLENAME);
-        if($geolocLimitSuccess){
+        if ($geolocLimitSuccess) {
 //            $establishmentsQuery->offset($sliceStart)->limit($nbElementPerPage);
             $establishmentsData = $establishmentsQuery->get();
             foreach ($establishmentsData as $establishmentData) {
@@ -65,13 +69,14 @@ class HomeController extends Controller
                 }
             }
         }
-             
-        $view =  View::make('front.home')->with('slider_ets', $sliderEts)->with('ds_selection', $establishments)
-                                        ->with('reloaded', \Illuminate\Support\Facades\Request::get('reload'));
+
+        $view = View::make('front.home')->with('slider_ets', $sliderEts)->with('ds_selection', $establishments)
+                ->with('reloaded', \Illuminate\Support\Facades\Request::get('reload'));
         $response = \Illuminate\Support\Facades\Response::make($view);
-        if(!empty(\Illuminate\Support\Facades\Cookie::queued('userLng'))){
+        if (!empty(\Illuminate\Support\Facades\Cookie::queued('userLng'))) {
             $response->withCookie(\Illuminate\Support\Facades\Cookie::queued('userLng'));
         }
         return $response;
     }
+
 }
