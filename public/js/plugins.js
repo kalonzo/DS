dac580d768984b79f7545c97feafaa4c807ed57d
Nico <nico.trendonline@gmail.com>
2017-08-08 09:47:00 +0200

    Update ETS (locationIndex, address, ets, logo)

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     if(isPluginLoaded($.fn.multiSelect)){
         $('.multiselect-dual').multiSelect();
     }
+    if(isPluginLoaded($.fn.fileinput)){
+        $(".bootstrap-file-input").fileinput({
+            'showUpload': false
+        });
+    }
 });
